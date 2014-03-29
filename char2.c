377007cb10ba6e30a0af6ed08e1c6793721f8b67
Miguel Santos Gonzalez <theblackhelena_232@hotmail.com>
2014-03-29 17:55:50 -0600

    Create char2.c

---
+++ char2.c
@@ -0,0 +1,21 @@
+/*	Sistemas programables
+	Alumno: Santos Gonzalez Miguel Angel
+	7-8 am
+	Tamaño de los tipos de datos
+*/
+
+# include <stdio.h>
+
+int main(){
+	char a;
+	int b;
+	long c;
+	float d;
+	double e;
+	printf("%d \n",sizeof(a));
+	printf("Char: %d %10 \n",sizeof(a));
+	printf("int: %d %10 \n",sizeof(b));
+	printf("long: %d %10 \n",sizeof(c));
+	printf("float: %d %10 \n",sizeof(d));
+	printf("double: %d %10 \n",sizeof(e));	
+}
