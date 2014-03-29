dd747835b81ea6b3aced633b29530749b90eabd5
Miguel Santos Gonzalez <theblackhelena_232@hotmail.com>
2014-03-29 17:55:08 -0600

    Create char.c

---
+++ char.c
@@ -0,0 +1,15 @@
+/*	Sistemas programables
+	Alumno: Santos Gonzalez Miguel Angel
+	7-8 am
+	Tamaño de los tipos de datos
+*/
+
+#include <stdio.h>
+
+int main(){
+	unsigned char a=183;
+	
+	printf("%d \n",(unsigned char)a);
+	printf("%c \n",(unsigned char)a);
+	printf("%u \n",(unsigned char) a);
+}
