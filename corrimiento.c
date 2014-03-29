1f2ae7d33180061dd84f0e2b67893cef4a979551
Miguel Santos Gonzalez <theblackhelena_232@hotmail.com>
2014-03-29 17:59:12 -0600

    Create corrimiento.c

---
+++ corrimiento.c
@@ -0,0 +1,15 @@
+/*	Sistemas programables
+	Alumno: Santos Gonzalez Miguel Angel
+	7-8 am
+	Corrimientos 
+*/
+
+# include <stdio.h>
+
+int main(){
+	char a=5;
+	printf("%d \n",a);
+	printf("%d \n",a<<1);
+	printf("%d \n",a<<3);
+	printf("%d \n",(a<<4)>>4);
+}
