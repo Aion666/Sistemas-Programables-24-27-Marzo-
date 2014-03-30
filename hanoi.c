74bd849c5d53b9ca896073585503a83abfab1ceb
Miguel Santos Gonzalez <theblackhelena_232@hotmail.com>
2014-03-29 18:24:50 -0600

    Create hanoi.c

---
+++ hanoi.c
@@ -0,0 +1,20 @@
+/*	Sistemas programables
+	Alumno: Santos Gonzalez Miguel Angel
+	7-8 am
+*/
+
+#include<stdio.h>
+int hanoi(int n){
+    if(n==1)
+        return 1;
+    else
+        return 2 * hanoi(n-1) + 1;
+}
+int main(){
+    int disc, mov;
+    printf("Torres de Hanoi \n");
+    printf("Numero de discos: ");
+	scanf("%i",&disc);
+    printf("\tNumero de Movimientos : %i\n", hanoi(disc));
+    return 0;
+}
